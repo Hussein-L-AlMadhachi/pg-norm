0619d3754a6f81c94d16ab568254c94b99068d92
Hussein <almadhachihussein@gmail.com>
2025-11-15 21:42:16 +0300

    fetching on auth + tighter ledger inforcement

--- src/tables.ts
+++ src/tables.ts
@@ -44,7 +44,7 @@ export class PG_Table implements TableBase {
         // validate keys
         for( const key of keys ){
             if ( ! this.visibles.includes(key) ){
-                throw new Error(`field ${key} has to be in visibles which is currectly: ${this.visibles.join(", ") }`);
+                throw new Error("inserted rows need to have all columns in visibles");
             }
         }
 
@@ -90,7 +90,7 @@ export class PG_Table implements TableBase {
         // validate keys
         for( const key of keys ){
             if ( ! this.visibles.includes(key) ){
-                throw new Error("updated has to be a fillable");
+                throw new Error("inserted rows need to have all columns in visibles");
             }
         }
 
@@ -110,6 +110,7 @@ export class PG_AuthTable extends PG_Table {
     protected passwordField:string = "password_hash";
     public table_name: string;
     protected identify_user_by: string;
+    private dummy_password_hash:string = "$2a$12$4jMZgsZF8HpkBKETdDSKDOIuFwkwYTppUbap/RbTyRCpFuHa2UoCe";
 
 
     constructor(pg_connection:PG_App , name: string, fillables: string[] = [] , identify_user_by:string="username" ) {
@@ -157,7 +158,7 @@ export class PG_AuthTable extends PG_Table {
 
 
     /**
-     * Verify password against stored hash
+     *   Verify password against stored hash (limited auth applications)
      */
     public async verifyPassword(user_identifier: string, plainTextPassword: string): Promise<boolean> {
         const [user] = await this.sql`
@@ -167,12 +168,61 @@ export class PG_AuthTable extends PG_Table {
         `;
 
         if (!user || !user[this.passwordField]) {
-            // Perform dummy comparison to prevent side channel and timing attacks (they're hardware exploits)
-            await bcrypt.compare('dummy_password', await this.hashPassword('dummy_password'));
+            // when the user doesn't exist we wanna act like we're trying to log the user in so they can't use timing attacks
+            await bcrypt.compare('dummy_password', this.dummy_password_hash);
             return false;
         }
 
         return await bcrypt.compare(plainTextPassword, user[this.passwordField]);
+    }
+
+
+    /**
+     *   A more practical method for authentication
+     */
+    public async idAfterAuth(user_identifier: string, plainTextPassword: string): Promise<number|undefined> {
+        const [user] = await this.sql`
+            SELECT ${this.sql(this.passwordField, "id")}
+            FROM ${this.sql(this.table_name)} 
+            WHERE ${this.identify_user_by} = ${user_identifier}
+        `;
+
+        if (!user || !user[this.passwordField]) {
+            // when the user doesn't exist we wanna act like we're trying to log the user in so they can't use timing attacks
+            await bcrypt.compare('dummy_password', this.dummy_password_hash);
+            return undefined;
+        }
+
+        if( await bcrypt.compare(plainTextPassword, user[this.passwordField]) ){
+            return user.id as number;
+        }
+    }
+
+
+    /**
+     *   More practical method for advanced Authentication methods
+     */
+    public async fetchAfterAuth(user_identifier: string, plainTextPassword: string , columns:string[] ): Promise<Record<string,any>|undefined> {
+        for ( const column of columns ){
+            if ( ! this.visibles.includes(column) ) throw new ReferenceError("access to non-viisble non-authorized columns is restricted")
+        }
+        
+        const [user] = await this.sql`
+            SELECT ${this.sql(this.passwordField, ...columns)}
+            FROM ${this.sql(this.table_name)} 
+            WHERE ${this.identify_user_by} = ${user_identifier}
+        `;
+
+        if (!user || !user[this.passwordField]) {
+            // when the user doesn't exist we wanna act like we're trying to log the user in so they can't use timing attacks
+            await bcrypt.compare('dummy_password', this.dummy_password_hash);
+            return undefined;
+        }
+
+        if( await bcrypt.compare(plainTextPassword, user[this.passwordField]) ){
+            delete user[this.passwordField];
+            return user;
+        }
     }
 
 
@@ -219,11 +269,71 @@ export class PG_Ledger implements TableBase {
                 FOR UPDATE
                 WITH CHECK (false);
 
+                CREATE POLICY ${ this.sql(`${this.table_name}_no_update_for_policy`) }
+                TO PUBLIC
+                FOR UPDATE USING (false);
+
+
                 CREATE POLICY ${ this.sql(`${this.table_name}_no_delete_policy`) } 
                 ON ${ this.sql(this.table_name) }
                 TO PUBLIC
                 FOR DELETE
                 USING (false);
+
+                CREATE OR REPLACE FUNCTION ${`${this.table_name}_block_all_updates()`}
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    RAISE EXCEPTION 'Table "%" is immutable. Updates are not allowed.', TG_TABLE_NAME;
+                    RETURN NULL;
+                END;
+                $$ LANGUAGE plpgsql;
+
+                -- Attach to your ledger table
+                CREATE TRIGGER ${ this.sql(`${this.table_name}_prevent_update_trigger`) } 
+                    BEFORE UPDATE ON ${this.table_name}
+                    FOR EACH ROW ALWAYS EXECUTE FUNCTION ${`${this.table_name}_block_all_updates()`};
+
+
+                REVOKE TRUNCATE ON ${this.table_name} FROM PUBLIC;
+
+                CREATE OR REPLACE FUNCTION ${`${this.table_name}_block_truncate()`}
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    RAISE EXCEPTION 'Table "%" is immutable. TRUNCATE is not allowed.', TG_TABLE_NAME;
+                    RETURN NULL;
+                END;
+                $$ LANGUAGE plpgsql;
+
+                -- Attach TRUNCATE trigger to your ledger table
+                CREATE TRIGGER ${ this.sql(`${this.table_name}_prevent_truncate_trigger`) }
+                    BEFORE TRUNCATE ON ${this.table_name}
+                    FOR EACH STATEMENT ALWAYS EXECUTE FUNCTION ${`${this.table_name}_block_truncate()`};
+
+
+                -- Creating a trigger function that blocks ALL deletes
+                CREATE OR REPLACE FUNCTION ${`${this.table_name}_block_all_deletes()`}
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    RAISE EXCEPTION 'Table "%" is immutable. DELETES are not allowed.', TG_TABLE_NAME;
+                    RETURN NULL;
+                END;
+                $$ LANGUAGE plpgsql;
+
+                -- Attach DELETE trigger to your ledger table
+                CREATE TRIGGER ${ this.sql(`${this.table_name}_prevent_any_deletes_trigger`) } 
+                    BEFORE DELETE ON ${this.table_name}
+                    FOR EACH ROW ALWAYS EXECUTE FUNCTION ${`${this.table_name}_block_all_deletes()`};
+
+                -- enabling ALWAYS on triggers
+                ALTER TABLE ${this.sql(this.table_name)}
+                ENABLE ALWAYS TRIGGER ${ this.sql(`${this.table_name}_prevent_update_trigger`) };
+
+                ALTER TABLE ${this.sql(this.table_name)}
+                ENABLE ALWAYS TRIGGER ${ this.sql(`${this.table_name}_prevent_truncate_trigger`) };
+
+                ALTER TABLE ${this.sql(this.table_name)}
+                ENABLE ALWAYS TRIGGER ${ this.sql(`${this.table_name}_prevent_any_deletes_trigger`) };
+
             `;
         }
 
